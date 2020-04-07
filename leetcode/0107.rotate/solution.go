10186dd6cb52202f93818bcbf937656e2fb1691a
Yeqi Tao <taoyeqi@hyperchain.cn>
2020-04-08 00:41:13 +0800

    feat(leetcode): add 0107 rotate solution

---
+++ leetcode/0107.rotate/solution.go
@@ -0,0 +1,39 @@
+package _107_rotate
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func rotate(matrix [][]int)  {
+	rotate0(matrix, 0, len(matrix)-1)
+}
+
+func rotate0(matrix [][]int, i, index int) {
+	if i >= index {
+		return
+	}
+	for k:=0; k<index-i; k++ {
+		matrix[i][i+k], matrix[k+i][index], matrix[index][index-k], matrix[index-k][i] = matrix[index-k][i], matrix[i][i+k], matrix[k+i][index], matrix[index][index-k]
+	}
+	i++
+	index--
+	rotate0(matrix, i, index)
+}
+
+func print(matrix [][]int) {
+	result := "[\n"
+	for _, a := range matrix {
+		r := "\t["
+		for i, v := range a {
+			r += strconv.FormatInt(int64(v), 10)
+			if i < len(a) - 1 {
+				r += ", "
+			}
+		}
+		r += "],\n"
+		result += r
+	}
+	result += "]"
+	fmt.Println(result)
+}
---
+++ leetcode/0107.rotate/solution_test.go
@@ -0,0 +1,25 @@
+package _107_rotate
+
+import (
+	"testing"
+)
+
+func TestRotate(t *testing.T) {
+	//matrix := [][]int{
+	//	{1,2,3},
+	//	{4,5,6},
+	//	{7,8,9},
+	//}
+	//rotate(matrix)
+	//print(matrix)
+
+	matrix1 := [][]int{
+		{5, 1, 9,11},
+		{2, 4, 8,10},
+		{13, 3, 6, 7},
+		{15,14,12,16},
+	}
+	print(matrix1)
+	rotate(matrix1)
+	print(matrix1)
+}
