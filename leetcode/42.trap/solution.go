0f612d9fdda5face87713993c7441e16cfac2a7e
Yeqi Tao <taoyeqi@hyperchain.cn>
2020-04-19 14:48:38 +0800

    feat(leetcode): add 42.trap solution

---
+++ leetcode/42.trap/solution.go
@@ -0,0 +1,49 @@
+package _2_trap
+
+func trap(height []int) int {
+	upStack := make([]int, 0)
+	result := 0
+	for i:=0; i< len(height);i++ {
+		if i == 0 {
+			goto hok
+		}
+		if height[i] > height[i-1] {
+			if i == 1 {
+				goto hok
+			}
+			for len(upStack) != 0 {
+				pre := upStack[len(upStack)-1]
+				size := (i - pre - 1) * calculate(height, pre, i)
+				result += size
+				if height[pre] > height[i] {
+					break
+				}
+				upStack = upStack[:len(upStack)-1]
+			}
+		}
+	hok:
+		if i == len(height) - 1 {
+			break
+		}
+		if height[i] > height[i+1] {
+			upStack = append(upStack, i)
+		}
+	}
+	return result
+}
+
+func calculate(height []int, s, e int) int {
+	max := -1
+	for i:=s+1;i<e;i++ {
+		if height[i] > max {
+			max = height[i]
+		}
+	}
+	min := -1
+	if height[s] > height[e] {
+		min = height[e]
+	} else {
+		min = height[s]
+	}
+	return min - max
+}
---
+++ leetcode/42.trap/solution_test.go
@@ -0,0 +1,11 @@
+package _2_trap
+
+import "testing"
+
+func TestTrap(t *testing.T) {
+	//a := []int{0,1,0,2,1,0,1,3,2,1,2,1}
+	//t.Log(trap(a))
+
+	a := []int{4,2,3}
+	t.Log(trap(a))
+}
